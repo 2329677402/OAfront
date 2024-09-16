a39cbac0f5da2d5b71e65289957068c921b0d0f0
Poco <xianglin20020315@163.com>
2024-09-16 16:58:51 +0800

    feat: 添加修改密码对话框

--- src/views/main/frame.vue
+++ src/views/main/frame.vue
@@ -5,14 +5,39 @@
  * @Description: OA系统首页
 -->
 <script setup name="frame">
-import { ref, computed } from "vue";
+import { ref, computed, reactive } from "vue";
 import { useAuthStore } from "@/stores/auth";
 import { Fold, Expand } from "@element-plus/icons-vue"; // 引入element-plus的图标
 import { useRouter } from "vue-router";
 
 const authStore = useAuthStore();
 const router = useRouter();
-let isCollapse = ref(false); // 折叠侧边栏菜单
+let isCollapse = ref(false); // 侧边栏菜单, 默认不折叠
+let dialogVisible = ref(false); // 修改密码对话框, 默认不显示
+let resetPwdform = reactive({
+  old_pwd: "",
+  new_pwd: "",
+  confirm_pwd: "",
+}); // 修改密码表单, 默认为空
+
+let formTag = ref(); // 获取重置密码的表单对象
+let rules = reactive({
+  old_pwd: [
+    // trigger: "blur"：失去输入框焦点时触发验证
+    { required: true, message: "请输入旧密码！", trigger: "blur" },
+    { min: 6, max: 20, message: "密码长度需在6~20之间！", trigger: "blur" },
+  ],
+  new_pwd: [
+    { required: true, message: "请输入新密码！", trigger: "blur" },
+    { min: 6, max: 20, message: "密码长度需在6~20之间！", trigger: "blur" },
+  ],
+  confirm_pwd: [
+    { required: true, message: "再次确认新密码！", trigger: "blur" },
+    { min: 6, max: 20, message: "密码长度需在6~20之间！", trigger: "blur" },
+  ],
+}); // 修改密码表单验证规则
+
+let formLabelWidth = "100px"; // 表单标签宽度
 
 let asideWidth = computed(() => {
   if (isCollapse.value) {
@@ -31,6 +56,27 @@ const onCollapseAside = () => {
 const onExit = () => {
   authStore.clearUserToken();
   router.push({ name: "login" });
+};
+
+// 提交密码表单
+const onSubmit = () => {
+  formTag.value.validate((valid, fields) => {
+    if (valid) {
+      console.log("验证通过");
+    } else {
+      console.log("验证失败");
+    }
+  });
+  console.log(fields);
+  type = "passward";
+};
+
+// 控制修改密码对话框
+const onControlResetPwdDialog = () => {
+  resetPwdform.old_pwd = "";
+  resetPwdform.new_pwd = "";
+  resetPwdform.confirm_pwd = "";
+  dialogVisible.value = true;
 };
 </script>
 
@@ -137,10 +183,12 @@ const onExit = () => {
           </span>
           <template #dropdown>
             <el-dropdown-menu>
-              <el-dropdown-item>修改密码</el-dropdown-item>
-              <el-dropdown-item divided @click="onExit"
-                >退出登录</el-dropdown-item
-              >
+              <el-dropdown-item @click="onControlResetPwdDialog">
+                修改密码
+              </el-dropdown-item>
+              <el-dropdown-item divided @click="onExit">
+                退出登录
+              </el-dropdown-item>
             </el-dropdown-menu>
           </template>
         </el-dropdown>
@@ -149,6 +197,43 @@ const onExit = () => {
       <el-main class="main">Main</el-main>
     </el-container>
   </el-container>
+
+  <!-- 修改密码对话框 -->
+  <el-dialog v-model="dialogVisible" title="修改密码" width="500">
+    <el-form :model="resetPwdform" :rules="rules" ref="formTag">
+      <el-form-item label="旧密码" :label-width="formLabelWidth" prop="old_pwd">
+        <el-input
+          v-model="resetPwdform.old_pwd"
+          autocomplete="off"
+          type="password"
+        />
+      </el-form-item>
+      <el-form-item label="新密码" :label-width="formLabelWidth" prop="new_pwd">
+        <el-input
+          v-model="resetPwdform.new_pwd"
+          autocomplete="off"
+          type="password"
+        />
+      </el-form-item>
+      <el-form-item
+        label="确认密码"
+        :label-width="formLabelWidth"
+        prop="confirm_pwd"
+      >
+        <el-input
+          v-model="resetPwdform.confirm_pwd"
+          autocomplete="off"
+          type="password"
+        />
+      </el-form-item>
+    </el-form>
+    <template #footer>
+      <div class="dialog-footer">
+        <el-button @click="dialogVisible = false">取消</el-button>
+        <el-button type="primary" @click="onSubmit"> 确认 </el-button>
+      </div>
+    </template>
+  </el-dialog>
 </template>
 
 <style scoped>
