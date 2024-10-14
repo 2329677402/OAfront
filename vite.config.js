8fdbb9de865fd1146376c181f80ed42a06589c51
Poco <xianglin20020315@163.com>
2024-10-15 02:22:18 +0800

    feat: 前端项目打包

--- vite.config.js
+++ vite.config.js
@@ -5,18 +5,37 @@
  * @Description:
  */
 import { fileURLToPath, URL } from "node:url";
-
-import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import VueDevTools from "vite-plugin-vue-devtools";
 import VueSetupExtend from "vite-plugin-vue-setup-extend";
+import { createHtmlPlugin } from "vite-plugin-html";
+import { defineConfig, loadEnv } from "vite";
+
+// 用于获取Vite中的环境变量
+const getViteEnv = (mode, target) => {
+  return loadEnv(mode, process.cwd())[target];
+};
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue(), VueDevTools(), VueSetupExtend()],
-  resolve: {
-    alias: {
-      "@": fileURLToPath(new URL("./src", import.meta.url)),
+export default ({ mode }) => {
+  return defineConfig({
+    plugins: [
+      vue(),
+      VueDevTools(),
+      VueSetupExtend(),
+      // 标签页title配置
+      createHtmlPlugin({
+        inject: {
+          data: {
+            title: getViteEnv(mode, "VITE_APP_TITLE"),
+          },
+        },
+      }),
+    ],
+    resolve: {
+      alias: {
+        "@": fileURLToPath(new URL("./src", import.meta.url)),
+      },
     },
-  },
-});
+  });
+};
